6bf4faa77c7aa11d711191f03be0d4a37c1c9e7e
Thant Htet Myet <thanthtetmyet1994@gmail.com>
2026-02-15 19:43:08 +0800

    Update Feature

--- orbit-vc-api/orbit-vc-api/PythonScripts/02_Monitor_VersionControl/restore_file.py
+++ orbit-vc-api/orbit-vc-api/PythonScripts/02_Monitor_VersionControl/restore_file.py
@@ -8,7 +8,7 @@ def restore_file(ip_address, dest_path, source_path):
     """
     Restore a file from local storage to remote UNC path.
 
-    Args:
+    Arguments:
         ip_address: IP address of the target machine
         dest_path: The destination path on the remote machine (e.g., C:/Users/...)
         source_path: The local source file path to copy from
